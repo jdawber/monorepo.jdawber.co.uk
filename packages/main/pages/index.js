b0aecc54169a73f1bd3b804b1bc8e3c307de8902
Jonathan Dawber <jonathan.dawber@weaveability.com>
2020-10-24 13:06:49 +0100

    Updates linkes and add main page

---
+++ packages/main/pages/index.js
@@ -0,0 +1,19 @@
+const IndexPage = () => {
+
+    return (
+        <>
+            <h1>Hello World from main</h1>
+            <ul>
+                <li>
+                    <a href="/one">App 1</a>
+                </li>
+                <li>
+                    <a href="/two">App 2</a>
+                </li>
+            </ul>     
+        </>   
+    )
+
+}
+
+export default IndexPage;
